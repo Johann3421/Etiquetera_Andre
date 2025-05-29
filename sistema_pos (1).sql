b9213952333dcbee739fff78a1de93202318bc6a
Johann3421 <1355430@senati.pe>
2025-05-28 20:04:35 -0500

    Subiendo cambios de bd

--- sistema_pos (1).sql
+++ sistema_pos (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-04-2025 a las 18:02:33
+-- Tiempo de generación: 29-05-2025 a las 03:04:09
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,16 +24,81 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `pcs_armadas`
+--
+
+CREATE TABLE `pcs_armadas` (
+  `id` int(11) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
+  `usuario` varchar(100) NOT NULL,
+  `gama` varchar(20) NOT NULL,
+  `cpu` varchar(100) NOT NULL,
+  `ram` varchar(255) NOT NULL,
+  `storage` varchar(255) NOT NULL,
+  `mb` varchar(100) NOT NULL,
+  `gpu` varchar(100) NOT NULL,
+  `psu` varchar(100) NOT NULL,
+  `case` varchar(100) NOT NULL,
+  `observaciones` text DEFAULT NULL,
+  `checklist_qa` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pcs_armadas`
+--
+
+INSERT INTO `pcs_armadas` (`id`, `fecha`, `usuario`, `gama`, `cpu`, `ram`, `storage`, `mb`, `gpu`, `psu`, `case`, `observaciones`, `checklist_qa`) VALUES
+(1, '2025-05-28 19:45:07', 'asd', 'baja', 'Intel Pentium G6400', '8GB DDR3 1600MHz', '120GB SSD', 'MSI A320M-A PRO', 'NVIDIA GT 1030', '400W Genérica', 'Caja Pequeña', '', '[{\"nombre\":\"CPU\",\"ok\":true,\"fail\":false,\"obs\":\"\"},{\"nombre\":\"RAM\",\"ok\":true,\"fail\":false,\"obs\":\"\"},{\"nombre\":\"STORAGE\",\"ok\":true,\"fail\":false,\"obs\":\"\"},{\"nombre\":\"MB\",\"ok\":true,\"fail\":false,\"obs\":\"\"},{\"nombre\":\"GPU\",\"ok\":true,\"fail\":false,\"obs\":\"\"},{\"nombre\":\"PSU\",\"ok\":true,\"fail\":false,\"obs\":\"\"},{\"nombre\":\"CASE\",\"ok\":true,\"fail\":false,\"obs\":\"\"}]');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recepciones`
+--
+
+CREATE TABLE `recepciones` (
+  `id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora_recibido` time NOT NULL,
+  `cliente_nombre` varchar(255) NOT NULL,
+  `cliente_telefono` varchar(20) DEFAULT NULL,
+  `equipo_tipo` varchar(100) DEFAULT NULL,
+  `equipo_password` varchar(100) DEFAULT NULL,
+  `diagnostico` varchar(255) DEFAULT NULL,
+  `extras` text DEFAULT NULL,
+  `observaciones` text DEFAULT NULL,
+  `fecha_entrega` date DEFAULT NULL,
+  `hora_entrega` time DEFAULT NULL,
+  `costo` decimal(10,2) DEFAULT NULL,
+  `tipo_moneda` varchar(10) DEFAULT NULL,
+  `foto_url` varchar(255) DEFAULT NULL,
+  `codigo_unico` varchar(20) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `codigo_generado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `recepciones`
+--
+
+INSERT INTO `recepciones` (`id`, `fecha`, `hora_recibido`, `cliente_nombre`, `cliente_telefono`, `equipo_tipo`, `equipo_password`, `diagnostico`, `extras`, `observaciones`, `fecha_entrega`, `hora_entrega`, `costo`, `tipo_moneda`, `foto_url`, `codigo_unico`, `created_at`, `codigo_generado`) VALUES
+(6, '2025-04-30', '20:32:00', 'dasds', 'asdasd', 'asdasd', 'asdasd', 'asdadsasd', '{\"cargador\":true,\"bateria\":true,\"bolsa\":false,\"estuche\":true,\"cable\":false}', 'adsasd', '2025-05-02', '11:38:00', 123.00, 'S/', 'images/foto_681250c97bf002.93106630.png', NULL, '2025-04-30 16:33:17', '00001A'),
+(7, '2025-04-30', '18:37:00', 'asdads', 'asdasd', 'asddas', 'asd', 'asdadssad', '{\"cargador\":true,\"bateria\":true,\"bolsa\":false,\"estuche\":true,\"cable\":false}', 'asasdasd', '2025-05-01', '14:40:00', 123.00, 'S/', 'images/foto_681251e6b26855.56361277.png', NULL, '2025-04-30 16:38:00', '00015A'),
+(8, '2025-05-27', '15:57:00', '', '', '', '', '', '{\"cargador\":false,\"bateria\":false,\"bolsa\":false,\"estuche\":false,\"cable\":false}', '', '0000-00-00', '00:00:00', 0.00, 'S/', '', NULL, '2025-05-27 20:57:29', '00091A');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `transactions`
 --
 
 CREATE TABLE `transactions` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `cliente` varchar(100) NOT NULL,
+  `cliente` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `total` decimal(10,2) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -44,11 +109,11 @@ CREATE TABLE `transactions` (
 CREATE TABLE `transaction_items` (
   `id` int(11) NOT NULL,
   `transaction_id` int(11) NOT NULL,
-  `descripcion` varchar(255) NOT NULL,
+  `descripcion` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `cantidad` int(11) NOT NULL,
   `precio` decimal(10,2) NOT NULL,
   `subtotal` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -58,22 +123,37 @@ CREATE TABLE `transaction_items` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`) VALUES
-(1, 'asd', 'asd@gmail.com', '$2y$10$TB0kB2YbYTN7O87V0slLF.OeI6KpXABTXVNTRSN/WyntBqIXa4hf.', '2025-04-24 17:28:29');
+(1, 'asd', 'asd@gmail.com', '$2y$10$TB0kB2YbYTN7O87V0slLF.OeI6KpXABTXVNTRSN/WyntBqIXa4hf.', '2025-04-24 17:28:29'),
+(2, 'johan', 'johan12@gmail.com', '$2y$10$bYPhA91twLRxIxonRaQFfOc7R0kU6w/udRhVhUuCxmgWJEi3VW5nW', '2025-04-24 23:30:50'),
+(3, 'Andersson Almerco', 'jhordan2402@gmail.com', '$2y$10$Sc9dxepCw2qWj2IbZg9QOeZt1wiBk4LvoXYcqSw/T9kv8kkSISghO', '2025-04-25 17:12:38'),
+(4, 'andre', 'andre@gmail.com', '$2y$10$C2bZDsJ8eNIE.QchDWedVeV0eKGRQvHuVpF1R54UetHupQ9PQ77C6', '2025-04-25 20:40:12');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `pcs_armadas`
+--
+ALTER TABLE `pcs_armadas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `recepciones`
+--
+ALTER TABLE `recepciones`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `transactions`
@@ -102,6 +182,18 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `pcs_armadas`
+--
+ALTER TABLE `pcs_armadas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `recepciones`
+--
+ALTER TABLE `recepciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT de la tabla `transactions`
 --
 ALTER TABLE `transactions`
@@ -117,7 +209,7 @@ ALTER TABLE `transaction_items`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
